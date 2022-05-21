dd20562978f6cf4710a180c6aa5f8b9da0ca4dc2
saniaalamgithub <saniaalam27@gamil.com>
2022-05-21 16:47:01 +0600

    day2 bug fixed

--- day2/index.js
+++ day2/index.js
@@ -14,7 +14,7 @@ con.connect(function (err) {
 });
 
 var studentTableCreateQuery =
-  "CREATE TABLE student (id INT, name VARCHAR(255), roll_no VARCHAR(255), address VARCHAR(255), section VARCHAR(255))";
+  "CREATE TABLE student (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), roll_no VARCHAR(255), address VARCHAR(255), section VARCHAR(255))";
   var studentInfoInsertion =
   "INSERT INTO student (id,name,roll_no,address,section) VALUES ('01', 'layla','1','narayangonj','A'),('02', 'sania','2','narayangonj','A'),('03', 'sinthy','3','narayangonj','A'),('04', 'sinji','4','narayangonj','A'),('05', 'ahona','5','narayangonj','A')";
 
@@ -23,21 +23,37 @@ app.get("/create", function (req, res) {
     con.query(studentTableCreateQuery, function (err, result) {
         if (err) throw err;
         console.log("Student created");
+        res.send("Good Job")
       });
 });
+
 app.get("/add",function(req,res){
     con.query(studentInfoInsertion,function(err,result){
         if(err)throw err;
         console.log("info inserted");
-
+        res.send("Good Job")
     })
 })
+
 app.get("/add/:name",function(req,res){
-    let xyz = "INSERT INTO student (id,name,roll_no,address,section) VALUES ('07', '"+req.params.name+"','7','narayangonj','B')"
+    let xyz = "INSERT INTO student (id,name,roll_no,address,section) VALUES ('', '"+req.params.name+"','7','narayangonj','B')"
     con.query(xyz,function(err,result){
         if(err)throw err;
+        res.send("Good Job")
     })
 })
-app.get("/")
+
+app.get("/add/:name/:roll/:area/:section",function(req,res){
+  let xyz = "INSERT INTO student (id,name,roll_no,address,section) VALUES ('', '"+req.params.name+"','"+req.params.roll+"','"+req.params.area+"','"+req.params.section+"')"
+  con.query(xyz,function(err,result){
+      if(err)throw err;
+      res.send("Good Job")
+  })
+})
+
+//localhost:3000/snigdha
+app.get("/snigdha",function(req,res){
+
+})
 
 app.listen(3000);
